68792cab5201ce925d6eeb3d79c38ee092ab2ca0
jaegwan <132431652+jaegwan0319@users.noreply.github.com>
2025-05-25 00:58:40 +0900

    Create 21300.cpp

---
+++ 21300.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(void){
+    int a,b,c,d,e,f,g;
+    int sum;
+    cin >> a>>b>>c>>d>>e>>f>>g;
+    sum = a+b+c+d+e+f+g;
+    cout << sum*5;
+    return 0;
+}
