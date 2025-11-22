86ff82b06cdb9b2582af6c5f8127c5183555f3c1
jaegwan <132431652+jaegwan0319@users.noreply.github.com>
2025-11-22 22:09:10 +0900

    251122까지의 백준 풀이

--- N과_M_9.cpp
+++ N과_M_9.cpp
@@ -5,42 +5,50 @@
 using namespace std;
 typedef pair<int, int> pa;
 
+//전역 변수 선언
+int arr[10];
 int N,M;
 vector<int> v;
-int visited[10]{};
 map<int, int> m;
 
 void DFS(int depth) {
     if(depth == M) {
         for(int i = 0;i<M;i++) cout << arr[i] << " ";
-        cout << "\n";
+        cout << '\n';
+        return;
     }
-
     else {
+        int prev = -1;
         for(int i = 0;i<N;i++) {
-            if(!visited[i]) {
-                visited[i] = true;
+            if(m[v[i]] != 0 && prev != v[i]) {
+                m[v[i]]--;
                 arr[depth] = v[i];
+                prev = v[i];
                 DFS(depth+1);
-                visited[i] = false;
+                m[v[i]]++;
             }
         }
     }
 }
 
+void solve(void) {
+    cin >> N >> M;
+    for(int i = 0;i<N;i++) {
+        int temp;
+        cin >> temp;
+        v.push_back(temp);
+        m[temp]++;
+    }
+    sort(v.begin(), v.end());
+
+    DFS(0);
+}
 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    cin >> N >> M;
-    v.resize(N);
-    for(int &i:v) {
-        cin >> i;
-        m[i]++;
-    }
-    sort(v.begin(),v.end());
-
-    DFS(0);
+    solve();
+    return 0;
 }
